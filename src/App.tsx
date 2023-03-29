c2054c0999f837b5901314c340e47f8eb24f59db
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-29 23:37:05 +0200

    loads data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
+import { useState, useEffect } from 'react';
 import './App.scss';
+import axios from 'axios';
+
+const url = 'https://gutendex.com/books/?search=berlin';
 
 function App() {
-	return <div className="App">
-	<h1>Debounce Example</h1>
-		<p>testing</p>
-	</div>;
+	const [books, setBooks] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		(async () => {
+			setIsLoading(true);
+			const data = (await axios.get(url)).data;
+			const _books = data.results;
+			setBooks(_books);
+			setIsLoading(false);
+		})();
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Search Gutenberg Books</h1>
+			<div className="searchArea">
+				Search: <input />
+			</div>
+			<hr />
+
+			{isLoading ? (
+				<p>loading...</p>
+			) : (
+				<h2>There are {books.length} books</h2>
+			)}
+		</div>
+	);
 }
 
 export default App;
