195cdbc1d86da74cd2391045299fd32de7e2859a
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-29 23:58:37 +0200

    shows titles

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { useState, useEffect } from 'react';
 import './App.scss';
 import axios from 'axios';
 
-const url = 'https://gutendex.com/books/?search=berlin';
+interface IBook {
+	id: number;
+	title: string;
+}
 
 function App() {
-	const [books, setBooks] = useState([]);
+	const [books, setBooks] = useState<IBook[]>([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [searchText, setSearchText] = useState('');
 
-	useEffect(() => {
+	const searchBooksApi = (_searchText: string) => {
+		const url = `https://gutendex.com/books/?search=${_searchText}`;
 		(async () => {
 			setIsLoading(true);
 			const data = (await axios.get(url)).data;
@@ -16,21 +21,36 @@ function App() {
 			setBooks(_books);
 			setIsLoading(false);
 		})();
-	}, []);
+	};
 
+	const handleSearchTextChange = (_searchText: string) => {
+		setSearchText(_searchText);
+		searchBooksApi(_searchText);
+	};
 	return (
 		<div className="App">
 			<div className="supertitle">Debouncing Example</div>
 			<h1>Search Gutenberg Books</h1>
 			<div className="searchArea">
-				Search: <input />
+				Search:{' '}
+				<input
+					autoFocus
+					onChange={(e) => handleSearchTextChange(e.target.value)}
+				/>
 			</div>
 			<hr />
 
 			{isLoading ? (
 				<p>loading...</p>
 			) : (
-				<h2>There are {books.length} books</h2>
+				<>
+					<h2>There are {books.length} books</h2>
+					<ul>
+						{books.map((book: IBook) => {
+							return <li>{book.title}</li>;
+						})}
+					</ul>
+				</>
 			)}
 		</div>
 	);
